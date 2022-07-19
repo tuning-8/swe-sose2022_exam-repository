1aa3e6aae3c4cb1078481027259d5aaf86243c36
Nico Schurig <nico-schurig@gmx.de>
2022-07-19 21:41:15 +0200

    added 'fileName','parserVersion','feedInformation','schemaInstance' and 'schemaLocation'

--- OpenMensa_Parser/OpenMensa_Parser/XmlWriter.cs
+++ OpenMensa_Parser/OpenMensa_Parser/XmlWriter.cs
@@ -1,15 +1,15 @@
 using System;
 using System.Xml;
+using System.Globalization;
 
 namespace OpenMensa_Parser
 {
     public class XmlWriter
     {
-        public string XmlFilePath {get; private set;}
-
-        public XmlWriter(string filePath)
-        {
-            this.XmlFilePath = filePath;
-        }
+        public static string fileName = "OpenmenMensaXml.xml";
+        public static string parserVersion = "1.01-1";
+        public static string feedInformation = "http://openmensa.org/open-mensa-v2";
+        public static string schemaInstance = "http://www.w3.org/2001/XMLSchema-instance";
+        public static string schemaLocation = "http://openmensa.org/open-mensa-v2.xsd";
     }
 }
