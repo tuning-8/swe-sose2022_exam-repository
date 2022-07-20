ecb26e20bd43d0c4b4ba74828134c12502d3edf2
Eric <tuning_8@gmx.de>
2022-07-20 21:59:33 +0200

    Updated the test for getNodesByAttribute

--- OpenMensa_Parser/OpenMensa_Parser/Parser.cs
+++ OpenMensa_Parser/OpenMensa_Parser/Parser.cs
@@ -12,8 +12,12 @@ namespace OpenMensa_Parser
             string menu_url = "https://www.studentenwerk-freiberg.de/freiberg/essen-trinken/speiseplan/de/woche/this/";
             string entry_node = "/html/body/div[1]/div[3]/div[2]/div[3]/div[2]/div/div[4]";
             HtmlParser testParser = new HtmlParser(menu_url, entry_node);
-            HtmlNode menuNode = testParser.getNodeByClassName("menu_day active");
-            testParser.printInnerText(menuNode, "");
+            List<HtmlNode> Dates = new List<HtmlNode>();
+            Dates = testParser.getNodesByAttribute(testParser.rootNode, "date", null);
+            foreach (HtmlNode date in Dates)
+            {
+                Console.WriteLine(date.GetAttributeValue("date", "").Trim());
+            }
         }
     }
 }
