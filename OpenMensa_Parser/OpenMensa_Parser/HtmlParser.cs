e5ba01052b8c392796c5dbd62c10f94bfc64f98f
Eric <tuning_8@gmx.de>
2022-07-20 21:42:27 +0200

    Restructured method getNodeByClassName

--- OpenMensa_Parser/OpenMensa_Parser/HtmlParser.cs
+++ OpenMensa_Parser/OpenMensa_Parser/HtmlParser.cs
@@ -28,6 +28,42 @@ namespace OpenMensa_Parser
             return Node;
         }
 
+        //function to parse a list of nodes downwards from the 'ParentNode', where the nodes attribute name equals the parameter
+        public List <HtmlAgilityPack.HtmlNode?> getNodesByAttribute(HtmlNode ParentNode, string AttributeName, string? AttributeValue)
+        {
+            List <HtmlAgilityPack.HtmlNode?> Nodes = new List <HtmlAgilityPack.HtmlNode?>();
+            //iteration through all direct child nodes (one hiarchal level) of the 'ParentNode'
+            foreach (var ChildNode in ParentNode.ChildNodes)
+            {
+                if (ChildNode.NodeType == HtmlNodeType.Element)
+                {
+                    //collect all attributes of the current child
+                    var Attributes = ChildNode.GetAttributes();
+                    foreach (var attribute in Attributes)
+                    {
+                        if (attribute.Name == AttributeName)
+                        {
+                            //returns the node, which has the proper 'AttributeValue' to the 'AttributeName'
+                            if (AttributeValue != null)
+                            {
+                                if (ChildNode.GetAttributeValue(AttributeName, "") == AttributeValue)
+                                {
+                                    Nodes.Add(htmlDoc.DocumentNode.SelectSingleNode((ChildNode.XPath).ToString()));
+                                }
+                            }
+                            //makeshift: if no specific attribute value is provided to the function it returns the first node matching the 'AttributeName'
+                            //TODO: overload function
+                            else
+                            {
+                                Nodes.Add(htmlDoc.DocumentNode.SelectSingleNode((ChildNode.XPath).ToString()));
+                            }
+                        }
+                    }
+                }
+            }
+            return Nodes;
+        }
+
         //function to print the inner text of a node with Null exception handling (only for test purpose)
         public void printInnerText(HtmlNode node, string def)
         {
