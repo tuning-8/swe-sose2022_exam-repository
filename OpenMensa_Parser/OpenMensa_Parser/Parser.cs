13c48e43b24e653c2c96d4748f69fd334a5b9969
Eric <tuning_8@gmx.de>
2022-07-20 11:01:40 +0200

    Tested methods 'getNodeByClassName' and 'printInnerText' from HtmlParser class

--- OpenMensa_Parser/OpenMensa_Parser/Parser.cs
+++ OpenMensa_Parser/OpenMensa_Parser/Parser.cs
@@ -8,7 +8,12 @@ namespace OpenMensa_Parser
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Test");
+            //manual tests for HtmlParser class:
+            string menu_url = "https://www.studentenwerk-freiberg.de/freiberg/essen-trinken/speiseplan/de/woche/this/";
+            string entry_node = "/html/body/div[1]/div[3]/div[2]/div[3]/div[2]/div/div[4]";
+            HtmlParser testParser = new HtmlParser(menu_url, entry_node);
+            HtmlNode menuNode = testParser.getNodeByClassName("menu_day active");
+            testParser.printInnerText(menuNode, "");
         }
     }
 }
