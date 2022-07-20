7bed166ae3a38704ed679d7c533a1ed59df74929
Eric <tuning_8@gmx.de>
2022-07-20 10:55:41 +0200

    Added new method getNodeByClassName

--- OpenMensa_Parser/OpenMensa_Parser/HtmlParser.cs
+++ OpenMensa_Parser/OpenMensa_Parser/HtmlParser.cs
@@ -10,6 +10,24 @@ namespace OpenMensa_Parser
         public HtmlDocument htmlDoc {get; private set;}
         HtmlNode rootNode;
 
+        //method to parse a single node, where the attribute name 'class' has the parameter 'className' as attribute value
+        public HtmlAgilityPack.HtmlNode? getNodeByClassName(string className)
+        {
+            HtmlAgilityPack.HtmlNode? Node = null;
+            foreach (var subMenuNode in this.rootNode.ChildNodes)
+            {
+                if (subMenuNode.NodeType == HtmlNodeType.Element)
+                {
+                    if (subMenuNode.GetAttributeValue("class", "") == className)
+                    {
+                        Console.WriteLine(subMenuNode.GetAttributeValue("date", ""));
+                        Node = htmlDoc.DocumentNode.SelectSingleNode((subMenuNode.XPath).ToString());
+                    }
+                }
+            }
+            return Node;
+        }
+
         public HtmlParser(string url, string node)
         {
             this.htmlURL = url;
