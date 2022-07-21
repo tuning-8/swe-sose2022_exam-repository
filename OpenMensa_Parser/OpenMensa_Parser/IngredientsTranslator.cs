a7b00f9c7341c647682850772385a86d9b11829a
Nico Schurig <nico-schurig@gmx.de>
2022-07-21 15:44:41 +0200

    creating the two dictionaries

--- OpenMensa_Parser/OpenMensa_Parser/IngredientsTranslator.cs
+++ OpenMensa_Parser/OpenMensa_Parser/IngredientsTranslator.cs
@@ -5,6 +5,12 @@ namespace OpenMena_Parser
 {
     static class IngredientsTranslator
     {
-        
+        private static Dictionary<int, string> IngredientsByIntIndicator = new Dictionary<int, string>() {
+
+        };
+
+        private static Dictionary<string, string> IngredientsByStringIndicator = new Dictionary<string, string>() {
+
+        };
     }
 }
