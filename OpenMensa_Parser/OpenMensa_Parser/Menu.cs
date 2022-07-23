fb1c775815df9ca6064e9ed84ad7ca7b4120f9be
Eric <tuning_8@gmx.de>
2022-07-23 14:16:57 +0200

    Implemented the category generation and Added comments

--- OpenMensa_Parser/OpenMensa_Parser/Menu.cs
+++ OpenMensa_Parser/OpenMensa_Parser/Menu.cs
@@ -9,13 +9,20 @@ namespace OpenMensa_Parser
         List<Weekday> weekdayList = new List<Weekday>();
         List<HtmlNode> weekdayNodes = new List<HtmlNode>();
 
+        //function to parse the dates -> weekdayNodes and create instances of Weekday -> weekdayList
         public void GenrateWeekdayInstances()
         {
+            //get all dates (class = 'menu_day', date=dd.mm.yyyy) of the week as nodes
             this.weekdayNodes = this.parserInstance.getNodesByAttribute(this.parserInstance.rootNode, "date");
 
-            foreach(HtmlNode weekdayNode in weekdayNodes)
+            //iterate through the date nodes
+            for (int i = 0; i<weekdayNodes.Count; i++)
             {
-                weekdayList.Add(new Weekday(parserInstance, weekdayNode));
+                //create a new instance of a weekday class foreach date in the list
+                //pass the parserInstance to the Weekday class
+                weekdayList.Add(new Weekday(parserInstance, weekdayNodes[i]));
+                //call the function parse the categories
+                weekdayList[i].GenerateCategoryInstances();
             }
         }
 
@@ -28,6 +35,7 @@ namespace OpenMensa_Parser
     public class Weekday
     {
         HtmlParser parserInstance;
+        HtmlNode weekdayNode;
         public string Date { get; set; }
         public List<Category> categoryList = new List<Category>();
         public List<HtmlNode> categoryNodes = new List<HtmlNode>();
@@ -35,18 +43,32 @@ namespace OpenMensa_Parser
         public Weekday(HtmlParser Parser, HtmlNode weekdayNode)
         {
             this.parserInstance = Parser;
+            this.weekdayNode = weekdayNode;
+            //extract the date string from the node
             this.Date = weekdayNode.GetAttributeValue("date", "");
         }
 
+        //function to parse the categories -> categoryNodes and create instances of Category -> categoryList
         public void GenerateCategoryInstances()
         {
+            //get all categories of the current weekday as nodes
+            this.categoryNodes = this.parserInstance.getNodesByAttribute(weekdayNode, "class", "category ");
 
+            //iterate through the category nodes
+            for (int i = 0; i<categoryNodes.Count; i++)
+            {
+                //pass the parserInstance to the Category class
+                categoryList.Add(new Category(parserInstance, categoryNodes[i]));
+                //call the function parse the categories
+                categoryList[i].GenerateDishInstances();
+            }
         }      
     }
 
     public class Category
     {
         HtmlParser parserInstance;
+        HtmlNode categoryNode;
         public string Name { get; set; }
         public List<Dish> dishList = new List<Dish>();
         public List<HtmlNode> dishNodes = new List<HtmlNode>();
@@ -54,7 +76,8 @@ namespace OpenMensa_Parser
         public Category(HtmlParser Parser, HtmlNode categoryNode)
         {
             this.parserInstance = Parser;
-
+            this.categoryNode = categoryNode;
+            this.Name = categoryNode.GetDirectInnerText().Trim();
         }
 
         public void GenerateDishInstances()
