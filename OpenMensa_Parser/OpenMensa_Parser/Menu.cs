ad91d8742cce5a17336b7ac9344fe360baf18b21
Nico Schurig <nico-schurig@gmx.de>
2022-07-23 12:37:05 +0200

    implemented basic structure with respect to UML-Diagram

--- OpenMensa_Parser/OpenMensa_Parser/Menu.cs
+++ OpenMensa_Parser/OpenMensa_Parser/Menu.cs
@@ -1,30 +1,63 @@
-﻿namespace OpenMensa_Parser
+﻿using System;
+using HtmlAgilityPack;
+
+namespace OpenMensa_Parser
 {
     public class Menu
     {
-        List<string> dateList = new List<string>();
+        public List<Weekday> weekdayList = new List<Weekday>();
+        public List<HtmlNode> weekdayNodes = new List<HtmlNode>();
 
-        List<string> ParseDate()
+        public void GenrateWeekdayInstances(HtmlParser testParser)
         {
-            List<string> dates = new List<string>();
-            return dates;
+
         }
     }
 
     public class Weekday
     {
-        struct DishStruct
-        {
-            string dishName;
-            double[] prices;
-            List<string> specialIngerdients;
-        }
-        struct CategoryStruct
-        {
-            string name;
-            List<DishStruct> dishList;
-        }
-        public string Date {get; private set;}
+        public string Date { get; set; }
+        public List<Category> categoryList = new List<Category>();
+        public List<HtmlNode> categoryNodes = new List<HtmlNode>();
 
+        public Weekday(HtmlNode weekdayNode)
+        {
+
+        }
+
+        public void GenerateCategoryInstances()
+        {
+
+        }      
+    }
+
+    public class Category
+    {
+        public string Name { get; set; }
+        public List<Dish> dishList = new List<Dish>();
+        public List<HtmlNode> dishNodes = new List<HtmlNode>();
+
+        public Category(HtmlNode categoryNode)
+        {
+
+        }
+
+        public void GenerateDishInstances()
+        {
+
+        }
+    }
+
+    public class Dish
+    {
+        public string DishName { get; set; }
+        public string[] Prices { get; set; }
+
+        public List<int> specialIngredients = new List<int>();
+
+        public Dish(HtmlNode dishNode)
+        {
+            
+        }
     }
 }
