a404d147541f2af86b63c563eb032cccff2da004
tuning-8 <tuning_8@gmx.de>
2022-07-25 14:52:37 +0200

    Changed the dictionaries to one string dictionary

--- OpenMensa_Parser/OpenMensa_Parser/IngredientsTranslator.cs
+++ OpenMensa_Parser/OpenMensa_Parser/IngredientsTranslator.cs
@@ -5,38 +5,35 @@ namespace OpenMena_Parser
 {
     static class IngredientsTranslator
     {
-        private static Dictionary<int, string> IngredientsByIntIndicator = new Dictionary<int, string>() {
-            {1,"mit Konservierungsstoffen"},
-            {2, "mit Farbstoff"},
-            {3, "gewachst"},
-            {4, "geschwärzt"},
-            {5, "mit Antioxidationsmittel"},
-            {6, "mit Phosphat"},
-            {7, "geschwefelt"},
-            {8, "mit Süßungsmitteln"},
-            {9, "mit Geschmacksverstärker"},
-            {10, "phenylalaninhaltig"},
-            {16, "chininhaltig"},
-            {18, "Weichtiere"},
-            {19, "glutenhaltig"},
-            {20, "Krebstiere"},
-            {21, "eihaltig"},
-            {22, "Erdnüsse"},
-            {23, "Soja"},
-            {24, "Milch/Milchzucker"},
-            {25, "Schalenfrüchte/Nüsse"},
-            {26, "Sellerie"},
-            {27, "Senf"},
-            {28, "Sesamsamen"},
-            {29, "Schwefeldioxid u. Sulfite"},
-            {30, "Fisch (Allergen)"},
-            {31, "Lupine"},
-            {35, "mit Azofarbstoff"},
-            {43, "mit kakaohaltiger Fettglasur"},
-            {171, "ungeeignet für Vegetarier"}
-        };
-
         private static Dictionary<string, string> IngredientsByStringIndicator = new Dictionary<string, string>() {
+            {"1","mit Konservierungsstoffen"},
+            {"2", "mit Farbstoff"},
+            {"3", "gewachst"},
+            {"4", "geschwärzt"},
+            {"5", "mit Antioxidationsmittel"},
+            {"6", "mit Phosphat"},
+            {"7", "geschwefelt"},
+            {"8", "mit Süßungsmitteln"},
+            {"9", "mit Geschmacksverstärker"},
+            {"10", "phenylalaninhaltig"},
+            {"16", "chininhaltig"},
+            {"18", "Weichtiere"},
+            {"19", "glutenhaltig"},
+            {"20", "Krebstiere"},
+            {"21", "eihaltig"},
+            {"22", "Erdnüsse"},
+            {"23", "Soja"},
+            {"24", "Milch/Milchzucker"},
+            {"25", "Schalenfrüchte/Nüsse"},
+            {"26", "Sellerie"},
+            {"27", "Senf"},
+            {"28", "Sesamsamen"},
+            {"29", "Schwefeldioxid u. Sulfite"},
+            {"30", "Fisch (Allergen)"},
+            {"31", "Lupine"},
+            {"35", "mit Azofarbstoff"},
+            {"43", "mit kakaohaltiger Fettglasur"},
+            {"171", "ungeeignet für Vegetarier"}
             {"WEI", "Weizen"},
             {"ROG", "Roggen"},
             {"GER", "Gerste"},
@@ -52,11 +49,6 @@ namespace OpenMena_Parser
             {"PIS", "Pistazien"},
             {"MAC", "Macadamia"}
         };
-
-        public static string TranslateIngredientIndicator(int indicatorInt)
-        {
-            return IngredientsByIntIndicator[indicatorInt];
-        }
 
         public static string TranslateIngredientIndicator(string indicatorString)
         {
