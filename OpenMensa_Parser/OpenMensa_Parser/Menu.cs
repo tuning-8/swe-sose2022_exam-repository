03a4be4551ff892aa93a9a2dc58e219f522828e7
tuning-8 <tuning_8@gmx.de>
2022-07-25 12:36:11 +0200

    First implementation of information storage

--- OpenMensa_Parser/OpenMensa_Parser/Menu.cs
+++ OpenMensa_Parser/OpenMensa_Parser/Menu.cs
@@ -57,8 +57,15 @@ namespace OpenMensa_Parser
             //iterate through the category nodes
             for (int i = 0; i<categoryNodes.Count; i++)
             {
-                //pass the parserInstance to the Category class
-                categoryList.Add(new Category(parserInstance, categoryNodes[i]));
+                try
+                {
+                    categoryList.Add(new Category(parserInstance, categoryNodes[i], categoryNodes[i+1]));
+                }
+                //the second bound for the last category is no category node but the last child of the date node
+                catch (System.ArgumentOutOfRangeException)
+                {
+                    categoryList.Add(new Category(parserInstance, categoryNodes[i], weekdayNode.LastChild));
+                }
                 //call the function parse the categories
                 categoryList[i].GenerateDishInstances();
             }
@@ -69,34 +76,73 @@ namespace OpenMensa_Parser
     {
         HtmlParser parserInstance;
         HtmlNode categoryNode;
+        HtmlNode nextCategoryNode;
         public string Name { get; set; }
         public List<Dish> dishList = new List<Dish>();
         public List<HtmlNode> dishNodes = new List<HtmlNode>();
 
-        public Category(HtmlParser Parser, HtmlNode categoryNode)
+        public Category(HtmlParser Parser, HtmlNode categoryNode, HtmlNode nextCategoryNode)
         {
             this.parserInstance = Parser;
             this.categoryNode = categoryNode;
+            this.nextCategoryNode =nextCategoryNode;
+            //extract the category string from the node
             this.Name = categoryNode.GetDirectInnerText().Trim();
         }
 
+        //function to parse the dishes -> dishNodes and create instances of Dish -> dishList
         public void GenerateDishInstances()
         {
-
+            //get all dishes of the current category as nodes
+            this.dishNodes = this.parserInstance.getNodesByNodeBounds(categoryNode.ParentNode, categoryNode, nextCategoryNode, "class", "row");
+            //iterate through the dish nodes
+            for (int i = 0; i<dishNodes.Count; i++)
+            {
+                dishList.Add(new Dish(parserInstance, dishNodes[i]));
+            }
         }
     }
 
     public class Dish
     {
         HtmlParser parserInstance;
+        HtmlNode dishNode;
         public string DishName { get; set; }
         public string[] Prices { get; set; }
 
-        public List<int> specialIngredients = new List<int>();
+        public List<string> specialIngredients = new List<string>();
 
         public Dish(HtmlParser Parser, HtmlNode dishNode)
         {
+            this.Prices = new string[4];
             this.parserInstance = Parser;
+            this.dishNode = dishNode;
+
+            foreach (HtmlNode dishTitle in this.parserInstance.getNodesByAttribute(this.dishNode, "class", "menu_title"))
+            {
+                this.DishName = dishTitle.GetDirectInnerText().Trim();
+            }
+            foreach (HtmlNode dishDescription in this.parserInstance.getNodesByAttribute(this.dishNode, "class", "description"))
+            {
+                if (dishDescription.ChildNodes[1].GetDirectInnerText().Trim() != "")
+                {
+                    this.DishName += " - " + dishDescription.ChildNodes[1].GetDirectInnerText().Trim();
+                }
+                foreach (HtmlNode dishCharacterisation in this.parserInstance.getNodesByAttribute(dishDescription, "class", "characterisation"))
+                {
+                    foreach (HtmlNode dishIngredient in this.parserInstance.getNodesByAttribute(dishCharacterisation, "title"))
+                    {
+                        this.specialIngredients.Add(dishIngredient.GetDirectInnerText().Trim());
+                    }
+                }
+                foreach (HtmlNode dishPrice in this.parserInstance.getNodesByAttribute(dishDescription, "class", "price"))
+                {
+                    foreach (HtmlNode dishPriceClass in this.parserInstance.getNodesByAttribute(dishPrice, "title"))
+                    {
+                        this.specialIngredients.Add(dishPriceClass.GetDirectInnerText().Trim());
+                    }
+                }
+            }
         }
     }
 }
